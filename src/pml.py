6db92ad2f969a3378ed93793252a87e09a68a2be
Aurélien SVEVI <sveviaurelien@gmail.com>
2016-11-20 22:06:40 +0100

    can write in a file + more password generated

--- src/pml.py
+++ src/pml.py
@@ -3,15 +3,15 @@ from Target import Target
 
 def init():
     print("PML")
-    firstname = input("Firstname: ")
-    lastname = input("lastname: ")
+    firstname = input("Firstname: ").lower()
+    lastname = input("lastname: ").lower()
     dateOfBirth = input("date of birth [DD/MM/YYYY] : ").split('/')
     postCode = input("post code : ")
 
     furtherInformation = []
     info = 'a'
     while(info!=''):
-        info = input("furtherInformation : ")
+        info = input("furtherInformation : ").lower()
         if(info != ''):
             furtherInformation.append(info)
 
@@ -20,43 +20,49 @@ def init():
     target = Target(firstname, lastname, dateOfBirth, postCode, furtherInformation)
     return target
 
-def generate(target,level=1):
+def generate(target,level,f):
     count = 0
     delimiter = ['-','.',' ','_', '/','']
     end = ['.','?','!','']
+    inter = ['']
     if target.postCode != '':
-        end.append(target.postCode[:2])
-        end.append(target.postCode)
-    if target.dateOfBirth != ['']:
-        end.append(target.dateOfBirth[2])
-        end.append(target.dateOfBirth[2][-2:])
+        inter.append(target.postCode[:2])
+        inter.append(target.postCode)
+    if (target.dateOfBirth != ['']) & (len(target.dateOfBirth) == 3):
+        inter.append(target.dateOfBirth[2])
+        inter.append(target.dateOfBirth[0]+target.dateOfBirth[1])
+        inter.append(target.dateOfBirth[2][-2:])
+        
 
     ### print basic password (only 1 parameter)
-    print (target.firstname)
-    print (target.lastname)
+    print (target.firstname,file=f)
+    print (target.lastname,file=f)
     count += 2
 
     ### generate basic password with first name and lastname
     for d in delimiter:
         for e in end:
-            print(target.firstname,d,target.lastname,e,sep='')
-            print(target.lastname,d,target.firstname,e,sep='')
-            count += 2
+            for i in inter:
+                print(target.firstname,d,target.lastname,i,e,file=f,sep='')
+                print(target.lastname,d,target.firstname,i,e,file=f,sep='')
+                count += 2
 
     print(count, "password generated")
 
 
     
 def main():
+    file = sys.stdout
+    level = 1
     target = init()
     for i in sys.argv:
         if(i == '-c'):
             commonPass = True
         elif(i.startswith('-o')):
-            file = i.split("=",2)[1]
+            file = open(i.split("=",2)[1],'w')
         elif(i.startswith('-l')):
             level = i.split("=",2)[1]
-    generate(target)
+    generate(target,level, file)
 
 
 if __name__ == "__main__":
