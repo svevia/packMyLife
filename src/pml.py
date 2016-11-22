e74136f85bad6010069231820f9eabbcd564388a
svevia <sveviaurelien@gmail.com>
2016-11-22 08:58:00 +0100

    Generate password from further informations and capitals letters

--- src/pml.py
+++ src/pml.py
@@ -20,11 +20,20 @@ def init():
     target = Target(firstname, lastname, dateOfBirth, postCode, furtherInformation)
     return target
 
-def generate(target,level,f):
+def generateBasic(target,level,f):
     count = 0
-    delimiter = ['','.',' ',',','-', '_','/',':']
+    delimiter = ['','.',' ',',']
     end = ['.','?','!','']
     inter = ['']
+
+    
+    if level >= 3:
+        delimiter.append('_')
+    if level >= 5:
+        delimiter.append('/')
+        delimiter.append(':')
+
+    
     if target.postCode != '':
         inter.append(target.postCode[:2])
         inter.append(target.postCode)
@@ -43,13 +52,41 @@ def generate(target,level,f):
     for d in delimiter:
         for e in end:
             for i in inter:
+                ## Without capital
                 print(target.firstname,d,target.lastname,i,e,file=f,sep='')
                 print(target.lastname,d,target.firstname,i,e,file=f,sep='')
-                count += 2
 
+                ## First letter of first word in capital
+                print(target.firstname.title(),d,target.lastname,i,e,file=f,sep='')
+                print(target.lastname.title(),d,target.firstname,i,e,file=f,sep='')
+
+                ## First letter of every word in capital
+                print(target.firstname.title(),d,target.lastname.title(),i,e,file=f,sep='')
+                print(target.lastname.title(),d,target.firstname.title(),i,e,file=f,sep='')
+
+                ## First letter of last word in capital
+                print(target.firstname,d,target.lastname.title(),i,e,file=f,sep='')
+                print(target.lastname,d,target.firstname.title(),i,e,file=f,sep='')
+                count += 8
+
+
+    count += generateFurther(target,level,f,end,inter)
     print(count, "password generated")
 
+def generateFurther(target,level,f,end,inter):
+    count = 0
+    for p in target.furtherInformation:
+        print(p,file=f,sep='')
+        print(p.title(),file=f,sep='')
+        count += 2
+        for e in end:
+            for i in inter:
+                print(p,i,e,file=f,sep='')
+                print(p.title(),i,e,file=f,sep='')
+                count += 2
 
+    return count
+    
     
 def main():
     file = sys.stdout
@@ -58,19 +95,20 @@ def main():
     i=1
     while i < len(sys.argv):
         if(sys.argv[i] == '-c'):#add common password to the list
-            commonPass = True
+            i+=1
+            commonPass = sys.argv[i]
         elif(sys.argv[i].startswith('-o')):#output in a file
             i+=1
             file = open(sys.argv[i],'w')
         elif(sys.argv[i].startswith('-l')):#change the level
             i+=1
-            level = sys.argv[i]
+            level = int(sys.argv[i])
         else:
             print("Don't know the option", sys.argv[i])
             sys.exit()
         i+=1
     target = init()
-    generate(target,level, file)
+    generateBasic(target,level, file)
 
 
 if __name__ == "__main__":
